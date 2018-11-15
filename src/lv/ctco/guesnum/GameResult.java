f2e54570d9065dd1c6e07791659e2b6568f75200
jpcelinc <44901033+jpcelinc@users.noreply.github.com>
2018-11-15 17:49:08 +0200

    Update GameResult.java
    version 2

--- src/lv/ctco/guesnum/GameResult.java
+++ src/lv/ctco/guesnum/GameResult.java
@@ -1,10 +1,13 @@
-package lv.ctco.guesnum;
+package lv.ctco.guessnum;
 
-/**
- * Created by yelena.pchelinceva on 11/13/2018.
- */
 public class GameResult {
     String name;
     int triesCount;
     long duration;
+
+
+    public void displayResult() {
+        System.out.printf("%s %d %.2f sec\n",
+                name,triesCount,duration/1000.0) ;
+    }
 }
